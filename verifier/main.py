d551dee2febd74663d4400f9a7e2854f5fd594ce
kalaiselvan <im.kalaiselvan.t@gmail.com>
2024-07-01 06:23:16 +0200

    imports adjustment

--- verifier/main.py
+++ verifier/main.py
@@ -1,10 +1,9 @@
 from verifier.common import *
 from verifier.optimization_functions import *
-from verifier.integrator_common import *
 from verifier.integrator_functions import *  
 
 if __name__ == "__main__":
     grammar_file = "/home/kalai/Development/Conspec_automation/specification_generator/conspec.tx"
-    specification_file = "/home/kalai/Development/Conspec_automation/specification_generator/auto_generated.cspec"
+    specification_file = "/home/kalai/Development/Conspec_automation/specification_generator/test.cspec"
 
-    result = Verification(grammar_file,specification_file,["cat"]).result
+    result = Verification(grammar_file,specification_file,["cat","car","dog","truck","airplane"]).result
